257e7bc978eb34f660c20fdaa2ae2183349a2c69
lovekesh <kumarmehlan@gmail.com>
2020-08-12 11:31:56 +0530

    added auto code for writer and other fixed lines that user shoud not see

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -22,8 +22,42 @@ module.exports.submitCode = function(req, res){
 }
 
 var createFile = function(data){
-    var updated = data.replace(/System.out/g, 'writer');
-    fs.appendFile('exp.java', updated, function (err) {
+    data = data.replace(/System.out/g, 'writer').replace(/  +/g, ' ');
+    var a = data.indexOf("void",0);
+    // to update the void functions
+    while(a != -1){
+        if(a == data.indexOf('void main(String[] args)')){
+            var b = data.indexOf('{', a);
+            var z = '\nwriter = new PrintWriter("the-file-name.txt", "UTF-8");\nwriter.println("entered Main function");';
+            var updated = data.slice(0,b+1) + z + data.slice(b+1);
+            data = updated;
+            a = data.indexOf("void",b+1);
+            // to add writer.close statement
+            var indexOfCloseBracket = data.lastIndexOf('}', data.lastIndexOf('}')-2);
+            data = data.slice(0, indexOfCloseBracket) + '\nwriter.close();\n' + data.slice(indexOfCloseBracket);
+        }else{
+            var b = data.indexOf('{', a);
+            var z = '\nwriter.println("entered new function and code is added");';
+            var updated = data.slice(0,b+1) + z + data.slice(b+1);
+            data = updated;
+            a = data.indexOf("void",b+1);
+        }
+        
+    }
+    // to add the import statement
+    if(data.search('import java.io.*')==-1){
+        var importStatement = 'import java.io.*;\n'
+        data = importStatement + data.slice(0);
+    }
+    
+    // to add printWriter
+    var printWriterStatement = '\n    public static PrintWriter writer; \n';
+    var indexOfPublicClassStatement = data.search('public class exp {');
+    data = data.slice(0, indexOfPublicClassStatement+18) + printWriterStatement + data.slice(indexOfPublicClassStatement+19);
+
+
+    console.log(data);
+    fs.appendFile('exp.java', data, function (err) {
         if (err) throw err;
         console.log('Saved!');
         executeFile();
@@ -48,6 +82,7 @@ var executeFile = function(){
                 console.log("File executed successfuly");
                 fs.unlinkSync('exp.java');
                 fs.unlinkSync('exp.class');
+                
             });
         }
         
